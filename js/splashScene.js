6510ea557c26de3cad768a7f21b9f99c3e283777
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2023-01-23 01:38:05 +0000

    game complete

---
+++ js/splashScene.js
@@ -0,0 +1,44 @@
+/* Global Phaser */
+//
+//Created by: Conor
+//Created on: Jan 2023
+//This is the Splash Scenes file
+
+/**
+ * This class is the Splash scene
+ */
+class SplashScene extends Phaser.Scene {
+  /**
+   * Method for constructor
+   */
+  constructor() {
+    super({ key: "splashScene" });
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
+  }
+  //loads splash scene and displays “Splash Scene” in the console
+  preload() {
+    console.log("Splash Scene");
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png");
+  }
+  //Code to display the splash scene
+  create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    );
+    this.splashSceneBackgroundImage.x = 1920 / 2;
+    this.splashSceneBackgroundImage.y = 1080 / 2;
+  }
+  //Switches scene to title scene after a delay
+  update(time, delta) {
+    if (time > 3000) {
+      this.scene.switch("titleScene");
+    }
+  }
+}
+
+export default SplashScene;
---
+++ js/titleScene.js
@@ -0,0 +1,58 @@
+/* Global Phaser */
+//
+//Created by: Conor
+//Created on: Jan 2023
+//This is the Title Scenes file
+
+/**
+ * This class is the Title scene
+ */
+class TitleScene extends Phaser.Scene {
+  /**
+   * Method for constructor
+   */
+  constructor() {
+    super({ key: "titleScene" });
+    this.titleSceneBackgroundImage = null;
+    this.titleSceneText = null;
+    this.titleSceneTextStyle = {
+      font: "200px Times",
+      fill: "#fde4b9",
+      allign: "center",
+    };
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
+  }
+
+  //loads title scene and displays "Title Scene" in the console
+  preload() {
+    console.log("Title Scene");
+    this.load.image(
+      "titleSceneBackground",
+      "./assets/aliens_screen_image2.jpg"
+    );
+  }
+
+  //Displays the loaded image
+  create(data) {
+    this.titleSceneBackgroundImage = this.add
+      .sprite(0, 0, "titleSceneBackground")
+      .setScale(2.75);
+    this.titleSceneBackgroundImage.x = 1920 / 2;
+    this.titleSceneBackgroundImage.y = 1080 / 2;
+    //Displays text on the screen
+    this.titleSceneText = this.add
+      .text(1920 / 2, 1080 / 2 + 350, "Dodge The Cars", this.titleSceneTextStyle)
+      .setOrigin(0.5);
+  }
+  //Changes scene after 6000 cycles
+  update(time, delta) {
+    if (time > 6000) {
+      this.scene.switch("menuScene");
+    }
+  }
+}
+
+export default TitleScene;
